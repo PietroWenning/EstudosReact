5446f8b88d779ed96968643e9cb3e86e11ab3979
Pietro calebe <pietrorepositorio@gmail.com>
2026-03-11 10:03:15 -0300

    Estudos React

--- DescrevendoaUI/JSXemChaves/my-app/src/App.jsx
+++ DescrevendoaUI/JSXemChaves/my-app/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// export default function Avatar() {
+//   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+//   const description = 'Gregorio Y. Zara';
+//   return (
+//     <img
+//       className="avatar"
+//       src={avatar}
+//       alt={description}
+//     />
+//   );
+// }
+// export default function TodoList() {
+//   const name = 'Gregorio Y. Zara';
+//   return (
+//     <h1>{name}'s To Do List</h1>
+//   );
+// }
 
+// const today = new Date();
+
+// function formatDate(date) {
+//   return new Intl.DateTimeFormat(
+//     'en-US',
+//     { weekday: 'long' }
+//   ).format(date);
+// }
+
+// export default function TodoList() {
+//   return (
+//     <h1>To Do List for {formatDate(today)}</h1>
+//   );
+// }
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+export default function TodoList() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
 }
-
-export default App
